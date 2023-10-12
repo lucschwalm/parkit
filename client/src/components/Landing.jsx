7c07e68482750626dd0f7b9efdf28d7a27ca6867
cbailey73 <123669370+cbailey73@users.noreply.github.com>
2023-10-12 18:49:54 -0500

    removed bad code from park search

--- client/src/components/Landing.jsx
+++ client/src/components/Landing.jsx
@@ -1,12 +1,40 @@
-
-const Landing = () => {
-
-    return (
+const ParkSearch = () => {
+    return(
         <div>
-            <img src="background.jpg" width="100%"/>
+  
         </div>
-
     )
-}
-
-export default Landing;
+  }
+  
+  export default ParkSearch;
+  
+  // import React from 'react';
+  // import { Link } from 'react-router-dom';
+  // import { useQuery } from '@apollo/client';
+  // import { QUERY_PARKS } from '../utils/queries'; 
+  
+  // function ParkList() {
+  //   const { loading, data } = useQuery(QUERY_PARKS);
+  
+  //   if (loading) {
+  //     return <div>Loading...</div>;
+  //   }
+  
+  //   const parks = data.parks;
+  
+  //   return (
+  //     <div className="container my-1">
+  //       <Link to="/">← Back to Home</Link>
+  
+  //       <h2>National Parks</h2>
+  //       {parks.map((park) => (
+  //         <div key={park._id} className="my-2">
+  //           <h3>{park.name}</h3>
+  //         </div>
+  //       ))}
+  //     </div>
+  //   );
+  // }
+  
+  // export default ParkList;
+  
