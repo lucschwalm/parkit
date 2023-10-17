66c535c12210ac3a14c8bce07a5a5dc4dfce667c
cbailey73 <123669370+cbailey73@users.noreply.github.com>
2023-10-17 18:10:38 -0500

    styling changes to profile

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import { Card, Container, Row, Col } from 'react-bootstrap';
+
 import { useQuery } from '@apollo/client';
 import { GET_USER } from '../utils/queries';
 
@@ -11,19 +13,27 @@ const Profile = () => {
   const user = data.me;
 
   return (
-    <div>
-      <h1>User Profile</h1>
-      <h2>Username: {user.userName}</h2>
-      <h2>Favorite Parks:</h2>
-      <ul>
-        {user.parks.map((park, index) => (
-          <li key={index}>
-            <h3>Name: {park.name}</h3>
-            <p>Description: {park.address}</p>
-          </li>
-        ))}
-      </ul>
-    </div>
+    <Container>
+      <Row>
+        <Col>
+          <Card className="mt-4">
+            <Card.Body className="mt-4 p-2">
+              <h1>My Profile</h1>
+              <h2>Username: {user.userName}</h2>
+              <h2>Favorite Parks:</h2>
+              <ul>
+                {user.parks.map((park, index) => (
+                  <li key={index}>
+                    <h3>Name: {park.name}</h3>
+                    <p>Description: {park.address}</p>
+                  </li>
+                ))}
+              </ul>
+            </Card.Body>
+          </Card>
+        </Col>
+      </Row>
+    </Container>
   );
 };
 
